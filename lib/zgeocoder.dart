bf49b15d5fd0bee7a5c680be4d1732053798d0ae
Zeus Cajurao <zeuscajurao@Zeuss-Mac-mini.local>
2025-01-29 00:36:46 +0800

    geocoder

--- lib/zgeocoder.dart
+++ lib/zgeocoder.dart
@@ -1,1 +1,18 @@
-export 'package:zgeocoder/src/google.dart';
+import 'package:zgeocoder/src/base.dart';
+import 'package:zgeocoder/src/google.dart';
+
+class Geocoder {
+  // static final Geocoding local = LocalGeocoding();
+  static Geocoding google({
+    String? language,
+    required String apiKey,
+    Map<String, Object>? headers,
+    bool preserveHeaderCase = false,
+  }) =>
+      GoogleGeocoding(
+        language: language,
+        headers: headers,
+        apiKey: apiKey,
+        preserveHeaderCase: preserveHeaderCase,
+      );
+}
